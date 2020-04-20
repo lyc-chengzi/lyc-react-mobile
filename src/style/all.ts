0da19a41b5a2e2825b797cc9a62bcb76a74c38d2
liuyc14 <liuyc14@lenovo.com>
2020-04-20 14:10:01 +0800

    compile success

---
+++ src/style/all.ts
@@ -0,0 +1,1 @@
+import '../components/LazyLoad/style/index.less';
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,52 @@
 const path = require('path');
-module.exports = {
-    entry: './src/index',
-    output: {
+const babelConfig = require('./babelrc.lib');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
+module.exports = {
+    mode: process.env.mode,
+    resolve: {
+        extensions: ['.ts', '.tsx', '.less', '.js', '.css']
+    },
+    entry: {
+        "lycReact-m": path.resolve(__dirname, 'src/index.ts'),
+        "css": path.resolve(__dirname, 'src/style/all.ts')
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: "[name].min.js"
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: 'style/[name].css',  // 打包后从js文件中提取出来的css文件名称
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    {loader: "style-loader"},
+                    {loader: "css-loader"}
+                ]
+            },
+            {
+                test: /\.less$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'less-loader'
+                ]
+            },
+            {
+                test: /\.([jt])sx$/,
+                // include: path.resolve(__dirname, 'src'),
+                use: [
+                    {
+                        loader: "babel-loader",
+                        options: babelConfig
+                    }
+                ]
+            }
+        ]
     }
 };
